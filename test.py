48127660d70cedfe6b60354ed3cab78c190150c1
Morskoikot <sacha.fed007@gmail.com>
2024-02-08 23:01:08 +0500

    QoL: Поменял форму добавочную и изменил названия кнопкам

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from PyQt6.QtWidgets import (
     QMainWindow,
 )
 from test_form import Ui_MainWindow as Register
-from Main_s import Ui_MainWindow as Main
+from Main_s import Ui_MainWindow as Form
 import Haha
 
 class AppWindow(QMainWindow):
@@ -20,12 +20,12 @@ class AppWindow(QMainWindow):
 class AppWindow_main(QMainWindow):
     def __init__(self):
         super(AppWindow_main, self).__init__()
-        self.ui = Main()
+        self.ui = Form()
         self.ui.setupUi(self)
-        self.ui.stackedWidget.setCurrentWidget(self.ui.page_3)
-        self.ui.pushButton_2.clicked.connect(self.transfer_page_add)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.Main_page)
+        self.ui.Button_Dobavit.clicked.connect(self.transfer_page_add)
     def transfer_page_add(self):
-        self.ui.stackedWidget.setCurrentWidget(self.ui.page_4)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.Add_Page)
 
 app = QApplication([])
 AppWindow_main = AppWindow_main()
